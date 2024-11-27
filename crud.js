379dc453ea970943e99d043129b1bdcd65fdbff0
Parth Lunagariya <124887250+lunagariya-parth@users.noreply.github.com>
2024-11-27 21:23:43 +0530

    fix Task update issue

--- crud.js
+++ crud.js
@@ -1,107 +1,106 @@
 const API_URL = "https://dummyjson.com/todos";
 
 const API_ENDPOINTS = {
-  FETCH: API_URL,
-  ADD: `${API_URL}/add`,
-  UPDATE: (id) => `${API_URL}/${id}`,
-  DELETE: (id) => `${API_URL}/${id}`,
+ FETCH: API_URL,
+ ADD: `${API_URL}/add`,
+ UPDATE: (id) => `${API_URL}/${id}`,
+ DELETE: (id) => `${API_URL}/${id}`,
 };
 
 function toggleTaskCompletion() {
-  const tasks = document.querySelectorAll(".task");
-  tasks.forEach((task) => {
-    const checkbox = task.querySelector('input[type="checkbox"]');
-    if (checkbox) {
-      checkbox.addEventListener("change", () => {
-        if (checkbox.checked) {
-          task.classList.add("task-completed");
-        } else {
-          task.classList.remove("task-completed");
-        }
-      });
+ const tasks = document.querySelectorAll(".task");
+ tasks.forEach((task) => {
+  const checkbox = task.querySelector('input[type="checkbox"]');
+  if (checkbox) {
+   checkbox.addEventListener("change", () => {
+    if (checkbox.checked) {
+     task.classList.add("task-completed");
+    } else {
+     task.classList.remove("task-completed");
     }
-  });
+   });
+  }
+ });
 }
 
 function toggleInputsState(container, disabled, selector = "input, button, select, textarea") {
-  const inputs = container.querySelectorAll(selector);
-  inputs.forEach((input) => {
-    input.disabled = disabled;
-  });
+ const inputs = container.querySelectorAll(selector);
+ inputs.forEach((input) => {
+  input.disabled = disabled;
+ });
 }
 
 // CRUD functions
 async function fetchTodos() {
-  showLoading();
-  try {
-    const response = await fetch(API_ENDPOINTS.FETCH);
-    const data = await response.json();
-    renderTodos(data.todos, "full-refresh");
-  } catch (error) {
-    showNotification("Error fetching todos", "error");
-    console.error(error);
-  } finally {
-    hideLoading();
-  }
+ showLoading();
+ try {
+  const response = await fetch(API_ENDPOINTS.FETCH);
+  const data = await response.json();
+  renderTodos(data.todos, "full-refresh");
+ } catch (error) {
+  showNotification("Error fetching todos", "error");
+  console.error(error);
+ } finally {
+  hideLoading();
+ }
 }
 
 async function addTodo(newTodo, isCompleted) {
-  const newTaskElement = document.getElementById("newTask");
-  toggleInputsState(newTaskElement, true);
+ const newTaskElement = document.getElementById("newTask");
+ toggleInputsState(newTaskElement, true);
 
-  try {
-    const response = await fetch(API_ENDPOINTS.ADD, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        todo: newTodo,
-        completed: isCompleted,
-        userId: 5,
-      }),
-    });
+ try {
+  const response = await fetch(API_ENDPOINTS.ADD, {
+   method: "POST",
+   headers: { "Content-Type": "application/json" },
+   body: JSON.stringify({
+    todo: newTodo,
+    completed: isCompleted,
+    userId: 5,
+   }),
+  });
 
-    const createdTodo = await response.json();
-    renderTodos([], "add", createdTodo);
-    showNotification("Task added successfully!", "success");
-  } catch (error) {
-    showNotification("Error adding task", "error");
-    console.error(error);
-  } finally {
-    toggleInputsState(newTaskElement, false);
-  }
+  const createdTodo = await response.json();
+  renderTodos([], "add", createdTodo);
+  showNotification("Task added successfully!", "success");
+ } catch (error) {
+  showNotification("Error adding task", "error");
+  console.error(error);
+ } finally {
+  toggleInputsState(newTaskElement, false);
+ }
 }
 
 async function editTodo(todoId, updatedText, isCompleted) {
-  const taskElement = document.querySelector(`.task[data-id="${todoId}"]`);
-  toggleInputsState(taskElement, true);
-  try {
-    const res = await fetch(API_ENDPOINTS.UPDATE(todoId), {
-      method: "PUT",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ todo: updatedText, completed: isCompleted }),
-    });
-    const data = await res.json();
-    if (data.message) {
-      throw new Error(" Task ID not Found");
-    }
-    showNotification("Task updated successfully!", "success");
-  } catch (error) {
-    throw new Error(error.message);
-  } finally {
-    toggleInputsState(taskElement, false);
+ const taskElement = document.querySelector(`.task[data-id="${todoId}"]`);
+ toggleInputsState(taskElement, true);
+ try {
+  const res = await fetch(API_ENDPOINTS.UPDATE(todoId), {
+   method: "PUT",
+   headers: { "Content-Type": "application/json" },
+   body: JSON.stringify({ todo: updatedText, completed: isCompleted }),
+  });
+  if (res.status != 200) {
+   throw new Error("Task ID is not Found");
   }
+  showNotification("Task updated successfully!", "success");
+ } catch (error) {
+  throw new Error(error.message);
+ } finally {
+  toggleInputsState(taskElement, false);
+ }
 }
 
 async function deleteTodo(todoId) {
-  const taskElement = document.querySelector(`.task[data-id="${todoId}"]`);
-  toggleInputsState(taskElement, true);
+ const taskElement = document.querySelector(`.task[data-id="${todoId}"]`);
+ toggleInputsState(taskElement, true);
 
-  try {
-    await fetch(API_ENDPOINTS.DELETE(todoId), { method: "DELETE" });
-    taskElement.remove();
-    showNotification("Task deleted successfully!", "success");
-  } catch (error) {
-    showNotification("Error deleting task", "error");
-    console.error(error);
-  }
+ try {
+  await fetch(API_ENDPOINTS.DELETE(todoId), { method: "DELETE" });
+  taskElement.remove();
+  showNotification("Task deleted successfully!", "success");
+ } catch (error) {
+  showNotification("Error deleting task", "error");
+  console.error(error);
+ }
 }
